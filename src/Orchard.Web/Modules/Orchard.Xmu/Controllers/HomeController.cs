e717d21127a5df85dde50d3e87808c8978de2283
tonychen <qingpengchen2011@gmail.com>
2016-08-04 21:24:07 +0800

    test viewbag

--- src/Orchard.Web/Modules/Orchard.Xmu/Controllers/HomeController.cs
+++ src/Orchard.Web/Modules/Orchard.Xmu/Controllers/HomeController.cs
@@ -10,9 +10,17 @@ namespace Orchard.Xmu.Controllers
 
     public class HomeController : Controller
     {
+        private readonly IOrchardServices _service;
+
+        public HomeController(IOrchardServices service)
+        {
+            _service = service;
+        }
+
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.hello = _service.WorkContext.CurrentSite.SiteName;
             return View();
         }
     }
