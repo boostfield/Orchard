ecdb25b9e9708525bc3cf34ae7504e403fd5affd
tonychen <qingpengchen2011@gmail.com>
2016-08-04 19:06:18 +0800

    xmu test module

---
+++ src/Orchard.Web/Modules/Orchard.Xmu/XmuRoutes.cs
@@ -0,0 +1,45 @@
+﻿using Orchard.Mvc.Routes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Orchard.Xmu
+{
+    public class XmuRoutes : IRouteProvider
+    {
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[]
+            {
+                new RouteDescriptor
+                {
+                  Priority = 100,
+
+            Route = new Route(
+             "home",
+             new RouteValueDictionary {
+                                        {"area", "Orchard.Xmu"},
+                                        {"controller", "Home"},
+                                        {"action", "Index"}
+                                       },
+             new RouteValueDictionary(),
+             new RouteValueDictionary {
+                                          {"area", "Orchard.Xmu"}
+                                       },
+             new MvcRouteHandler())
+                }
+            };
+        }
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+
+    }
+}
