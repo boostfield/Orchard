00ad1288251b7ab49305e2456b1083fd68ff5475
tonychen <qingpengchen2011@gmail.com>
2016-08-17 15:54:04 +0800

    获取菜单项

---
+++ src/Orchard.Web/Modules/Orchard.Xmu/Models/MenuGroupResultVM.cs
@@ -0,0 +1,14 @@
+﻿using Orchard.Core.Navigation.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Orchard.Xmu.Models
+{
+    public class MenuGroupResultVM
+    {
+        public char Position { get; set; }
+        public IList<MenuItemEntry> List { get; set; }
+    }
+}
