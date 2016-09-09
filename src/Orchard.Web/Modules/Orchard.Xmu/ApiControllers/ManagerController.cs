f4245694a75e86e3f61c05b300defb18cf4f4c1e
tonychen <qingpengchen2011@gmail.com>
2016-09-09 11:40:43 +0800

    科研数据的搜索api

--- src/Orchard.Web/Modules/Orchard.Xmu/ApiControllers/ManagerController.cs
+++ src/Orchard.Web/Modules/Orchard.Xmu/ApiControllers/ManagerController.cs
@@ -56,5 +56,149 @@ namespace Orchard.Xmu.ApiControllers
 
 
         }
+
+        [HttpGet]
+        [ActionName("SearchCNTeacher")]
+        public IHttpActionResult SearchCNTeacher([FromUri] string keyword, [FromUri]PagerParameters pagerParameters)
+        {
+            keyword = keyword.Trim();
+            Pager pager = new Pager(_siteService.GetSiteSettings(), pagerParameters);
+            var q = _contentManager.Query<CNTeacherPart, CNTeacherPartRecord>(VersionOptions.Latest)
+                .Where<CNTeacherPartRecord>(i => i.Name.Contains(keyword))
+                .OrderByDescending(i => i.Id);
+
+            var total = q.Count();
+            var items = q.Slice(pager.GetStartIndex(), pager.PageSize)
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Number = p.Number
+                });
+
+            return Json(new ApiListResponse
+            {
+                DataList = items,
+                TotalCount = total,
+                PageSize = pager.PageSize,
+                PageNumber = pager.Page
+            });
+
+
+        }
+
+
+        [HttpGet]
+        [ActionName("SearchAcademicPaper")]
+        public IHttpActionResult SearchAcademicPaper([FromUri] string keyword, [FromUri]PagerParameters pagerParameters)
+        {
+            keyword = keyword.Trim();
+            Pager pager = new Pager(_siteService.GetSiteSettings(), pagerParameters);
+            var q = _contentManager.Query<AcademicPaperPart, AcademicPaperRecord>(VersionOptions.Latest)
+                .Where<AcademicPaperRecord>(i => i.Title.Contains(keyword))
+                .OrderByDescending(i => i.Id);
+
+            var total = q.Count();
+            var items = q.Slice(pager.GetStartIndex(), pager.PageSize)
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Title = p.Title
+                });
+
+            return Json(new ApiListResponse
+            {
+                DataList = items,
+                TotalCount = total,
+                PageSize = pager.PageSize,
+                PageNumber = pager.Page
+            });
+        }
+
+        [HttpGet]
+        [ActionName("SearchAcademicWork")]
+        public IHttpActionResult SearchAcademicWork([FromUri] string keyword, [FromUri]PagerParameters pagerParameters)
+        {
+            keyword = keyword.Trim();
+            Pager pager = new Pager(_siteService.GetSiteSettings(), pagerParameters);
+            var q = _contentManager.Query<AcademicWorksPart, AcademicWorksRecord>(VersionOptions.Latest)
+                .Where<AcademicWorksRecord>(i => i.Title.Contains(keyword))
+                .OrderByDescending(i => i.Id);
+
+            var total = q.Count();
+            var items = q.Slice(pager.GetStartIndex(), pager.PageSize)
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Title = p.Title
+                });
+
+            return Json(new ApiListResponse
+            {
+                DataList = items,
+                TotalCount = total,
+                PageSize = pager.PageSize,
+                PageNumber = pager.Page
+            });
+
+        }
+
+
+        [HttpGet]
+        [ActionName("SearchAward")]
+        public IHttpActionResult SearchAward([FromUri] string keyword, [FromUri]PagerParameters pagerParameters)
+        {
+            keyword = keyword.Trim();
+            Pager pager = new Pager(_siteService.GetSiteSettings(), pagerParameters);
+            var q = _contentManager.Query<AwardsPart, AwardsRecord>(VersionOptions.Latest)
+                .Where<AwardsRecord>(i => i.AwardName.Contains(keyword))
+                .OrderByDescending(i => i.Id);
+
+            var total = q.Count();
+            var items = q.Slice(pager.GetStartIndex(), pager.PageSize)
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    AwardName = p.AwardName
+                });
+
+            return Json(new ApiListResponse
+            {
+                DataList = items,
+                TotalCount = total,
+                PageSize = pager.PageSize,
+                PageNumber = pager.Page
+            });
+
+        }
+
+        [HttpGet]
+        [ActionName("SearchProject")]
+        public IHttpActionResult SearchProject([FromUri] string keyword, [FromUri]PagerParameters pagerParameters)
+        {
+            keyword = keyword.Trim();
+            Pager pager = new Pager(_siteService.GetSiteSettings(), pagerParameters);
+            var q = _contentManager.Query<ProjectPart, ProjectRecord>(VersionOptions.Latest)
+                .Where<ProjectRecord>(i => i.ProjectTitle.Contains(keyword))
+                .OrderByDescending(i => i.Id);
+
+            var total = q.Count();
+            var items = q.Slice(pager.GetStartIndex(), pager.PageSize)
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Title = p.ProjectTitle
+                });
+
+            return Json(new ApiListResponse
+            {
+                DataList = items,
+                TotalCount = total,
+                PageSize = pager.PageSize,
+                PageNumber = pager.Page
+            });
+
+        }
+
     }
 }
