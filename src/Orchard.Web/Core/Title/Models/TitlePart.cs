51864a274f5ac8f1892cf8f28565d0a66b2d0bce
qky <qky1412@gmail.com>
2017-01-03 11:48:48 +0800

    TitlePart的标题不为required

--- src/Orchard.Web/Core/Title/Models/TitlePart.cs
+++ src/Orchard.Web/Core/Title/Models/TitlePart.cs
@@ -4,10 +4,9 @@ using Orchard.ContentManagement.Aspects;
 
 namespace Orchard.Core.Title.Models {
     public class TitlePart : ContentPart<TitlePartRecord>, ITitleAspect {
-        [Required]
         public string Title {
-            get { return Retrieve(x => x.Title); }
-            set { Store(x => x.Title, value); }
+            get { return Retrieve(x => x.Title) ?? string.Empty; }
+            set { Store(x => x.Title, value!=null ? value : string.Empty); }
         }
     }
 }
