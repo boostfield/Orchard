309534b3e0abda37b1c4aa7f482a9f66fc478bfb
qky <qky1412@gmail.com>
2017-01-18 17:14:35 +0800

    reenable media part

--- src/Orchard.Web/Modules/Orchard.MediaLibrary/Migrations.cs
+++ src/Orchard.Web/Modules/Orchard.MediaLibrary/Migrations.cs
@@ -103,6 +103,40 @@ namespace Orchard.MediaLibrary {
             return 7;
         }
 
+        public int UpdateFrom7()
+        {
+            ContentDefinitionManager.AlterPartDefinition("MediaPart", part => part
+              .Attachable()
+              .WithDescription("Turns a content type into a Media. Note: you need to set the stereotype to \"Media\" as well.")
+          );
+
+            ContentDefinitionManager.AlterPartDefinition("ImagePart", part => part
+                            .Attachable()
+                            .WithDescription("Provides common metadata for an Image Media.")
+                        );
+
+            ContentDefinitionManager.AlterPartDefinition("VideoPart", part => part
+                            .Attachable()
+                            .WithDescription("Provides common metadata for a Video Media.")
+                        );
+
+            ContentDefinitionManager.AlterPartDefinition("AudioPart", part => part
+                            .Attachable()
+                            .WithDescription("Provides common metadata for an Audio Media.")
+                        );
+
+            ContentDefinitionManager.AlterPartDefinition("DocumentPart", part => part
+                            .Attachable()
+                            .WithDescription("Provides common metadata for a Document Media.")
+                        );
+
+            ContentDefinitionManager.AlterPartDefinition("OEmbedPart", part => part
+                            .Attachable()
+                            .WithDescription("Provides common metadata for an OEmbed Media.")
+                        );
+            return 8;
+        }
+
         public int UpdateFrom2() {
             ContentDefinitionManager.AlterTypeDefinition("Image", td => td
                 .WithIdentity()
